65a51c744b18a60fb53f48c9fe63c14986d6ba9d
HajimeOgata <mejiha18@yahoo.co.jp>
2017-04-24 08:02:34 +0000

    revision new2

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,5 @@ Task.create(status: 'test title 1', content: 'test content 1')
 Task.create(status: 'test title 2', content: 'test content 2')
 Task.create(status: 'test title 3', content: 'test content 3')
 (1..100).each do |number|
-  Message.create(title: 'test title ' + number.to_s, content: 'test content ' + number.to_s)
+  Message.create(status: 'test title ' + number.to_s, content: 'test content ' + number.to_s)
 end
